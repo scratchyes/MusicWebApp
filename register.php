75d7b9cf46bfe711e7386553b20eaafce0108e93
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2017-12-14 11:01:56 +0530

    Register Input Values Retention Function Due To An Error Is Added

--- register.php
+++ register.php
@@ -11,6 +11,17 @@ include("includes/classes/Constants.php");
     include("includes/handlers/login-handler.php");
 
 
+    function getInputValue($name){
+        
+        if(isset($_POST[$name])){
+            
+            echo $_POST[$name];
+            
+        }
+        
+    }
+
+
 ?>
 
 
@@ -37,7 +48,13 @@ include("includes/classes/Constants.php");
 			<button type="submit" name="loginButton">LOG IN</button>
 			
 		</form>
+        
 
+        
+                        <!--  REGISTER FORM      -->
+        
+        
+        
 
 
 		<form id="registerForm" action="register.php" method="POST">
@@ -45,19 +62,19 @@ include("includes/classes/Constants.php");
 			<p>
 				<?php echo $account->getError(Constants::$userNameCharacters); ?>
 				<label for="username">Username</label>
-				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" required>
+				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
 			</p>
 
 			<p>
 				<?php echo $account->getError(Constants::$firstNameCharacters); ?>
 				<label for="firstName">First name</label>
-				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
 			</p>
 
 			<p>
 				<?php echo $account->getError(Constants::$lastNameCharacters); ?>
 				<label for="lastName">Last name</label>
-				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
 			</p>
 
 			<p>
@@ -66,12 +83,12 @@ include("includes/classes/Constants.php");
                 
                 
 				<label for="email">Email</label>
-				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
+				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
 			</p>
 
 			<p>
 				<label for="email2">Confirm email</label>
-				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" required>
+				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
 			</p>
 
 			<p>
