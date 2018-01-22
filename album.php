eb751cc3c1bf7bf6b9dcd07697f36b9d6e30ef36
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-01-22 14:09:37 +0530

    Added classes for retrieving album/artist info in the album page

--- album.php
+++ album.php
@@ -1,4 +1,32 @@
-<?php include("includes/header.php"); ?>
+<?php include("includes/header.php"); 
+
+
+
+
+if(isset($_GET['id'])){
+    
+    $albumId = $_GET['id'];
+    
+    
+}
+else{
+    
+   header("Location: index.php");
+    
+}
+
+
+
+$album = new Album($con, $albumId);
+
+$artist = $album->getArtist();
+
+
+
+echo $album->getTitle() . "<br>";
+echo $artist->getName();
+
+?>
                 
                 
 
