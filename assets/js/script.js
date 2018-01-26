b16b65e662d771c4e8a52be6f84bee6629c80a9d
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-01-26 23:12:27 +0530

    Add PlayBack Progress Bar Drag Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,80 @@
 var currentPlaylist = [];
 var audioElement;
+var mouseDown = false;
+
+
+function formatTime(seconds){
+    
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time / 60);
+    var seconds = time - (minutes*60);
+    
+    var extraZero;
+    
+    if(seconds<10){
+        
+        extraZero = "0";
+        
+    }
+    
+    else{
+        extraZero ="";
+        
+    }
+    
+    
+    return minutes + ":" + extraZero +seconds;
+    
+}
+
+
+function updateTimeProgressBar(audio){
+    
+    $(".progressTime.current").text(formatTime(audio.currentTime));
+   $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+    
+    
+    var progress = (audio.currentTime / audio.duration) *100;
+    
+    $(".playbackBar .progress").css("width", progress + "%");
+    
+    
+    
+}
+
+
+
 
 
 function Audio() {
 
 	this.currentlyPlaying;
 	this.audio = document.createElement('audio');
+    
+    this.audio.addEventListener("canplay", function(){
+        
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
 
+    });
+
+    
+   this.audio.addEventListener("timeupdate", function(){
+       
+    
+       
+       
+    if(this.duration){
+        updateTimeProgressBar(this);
+   
+    }
+       
+       
+   });
+    
+    
+    
+    
 	this.setTrack = function(track) {
         
         this.currentlyPlaying = track;
@@ -26,6 +94,15 @@ function Audio() {
         this.audio.pause();
         
     }
+    
+    
+    this.setTime = function(seconds){
+        this.audio.currentTime = seconds;
+        
+    }
+    
+    
+    
     
 
 }
